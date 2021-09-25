5a93448908a62a295ffda1e3772044058cfb89bc
arne-vanmarcke <r0783508@student.vives.be>
2021-09-25 13:32:33 +0200

    Setup

---
+++ RPI/Build/CMakeFiles/RPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPI.dir/RPI.c.o"
+  "libRPI.a"
+  "libRPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
