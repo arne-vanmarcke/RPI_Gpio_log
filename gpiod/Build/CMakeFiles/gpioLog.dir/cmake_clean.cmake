c4ba063f2ea1082547c1c91c6b20a27cb628cc9e
arne-vanmarcke <r0783508@student.vives.be>
2021-09-28 18:14:10 +0200

    Build project

---
+++ gpiod/Build/CMakeFiles/gpioLog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpioLog.dir/main.c.o"
+  "gpioLog"
+  "gpioLog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gpioLog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
